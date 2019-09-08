e4ae9c065982e09316a0a08ba3520c5dadd7bafd
Aswin G <aswinganesh666@gmail.com>
2019-09-08 18:06:21 +0530

    Add code to initialize n states

--- src/main.go
+++ src/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+// State represents a single state in the DFA
+type State struct {
+	name        string
+	isInitial   bool
+	isFinal     bool
+	transitions map[string]State
+}
+
+// Global variables
+var vocab string
+var n int
 
 func main() {
-	fmt.Println("hello")
+	fmt.Println("Enter Vocabulary")
+	fmt.Scanf("%s\n", &vocab)
+	fmt.Print("Enter number of states\n")
+	fmt.Scanf("%d\n", &n)
+	states := initStates()
+}
+
+func initStates() (states []State) {
+	for i := 0; i < n; i++ {
+		states = append(states, State{
+			name:      "q" + strconv.Itoa(i+1),
+			isFinal:   false,
+			isInitial: false,
+		})
+	}
+	return
 }
