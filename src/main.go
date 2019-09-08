46bdc0a5b85fb1374a7050648dd36fc596c084f9
Aswin G <aswinganesh666@gmail.com>
2019-09-08 18:21:30 +0530

    Use hashmap to store states instead of array

--- src/main.go
+++ src/main.go
@@ -16,22 +16,28 @@ type State struct {
 // Global variables
 var vocab string
 var n int
+var states = make(map[string]State)
 
 func main() {
 	fmt.Println("Enter Vocabulary")
 	fmt.Scanf("%s\n", &vocab)
 	fmt.Print("Enter number of states\n")
 	fmt.Scanf("%d\n", &n)
-	states := initStates()
+	states = initStates()
 }
 
-func initStates() (states []State) {
+func initStates() (states map[string]State) {
 	for i := 0; i < n; i++ {
-		states = append(states, State{
-			name:      "q" + strconv.Itoa(i+1),
-			isFinal:   false,
-			isInitial: false,
-		})
+		states["q"+strconv.Itoa(i+1)] = State{
+			name:        "q" + strconv.Itoa(i+1),
+			isFinal:     false,
+			isInitial:   false,
+			transitions: make(map[string]State),
+		}
 	}
 	return
+}
+
+func getStateNameFromID(i int) string {
+	return "q" + strconv.Itoa(i+1)
 }
